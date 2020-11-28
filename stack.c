2be89953c937799205ad3f0c5fc509a13d255f07
vienamargus <68496793+Viena78@users.noreply.github.com>
2020-11-28 20:18:20 +0700

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,105 @@
+
+#include "stack.h"
+#include <stdio.h>
+
+/* *** Konstruktor/Kreator *** */
+void CreateEmpty(Stack *S){
+/* I.S. Sembarang */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* Ciri stack kosong : TOP bernilai Nil */
+    (*S).TOP = Nil;
+}
+
+boolean IsEmpty(Stack S){
+/* Mengirim true jika Stack kosong*/
+/* Ciri stack kosong : TOP bernilai Nil */
+    return ( (S).TOP == Nil);
+}
+
+boolean IsFull(Stack S){
+/* Mengirim true jika stack S penuh */
+/* Ciri stack penuh : TOP bernilai MaxEl */
+    return ( (S).TOP == MaxEl);
+}
+
+/* ********** Operator Dasar Stack ********* */
+void Push(Stack *S, infotype X){
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, S tidak penuh */
+/* F.S. X menjadi element TOP yang baru, TOP bertambah 1 */
+    if(!IsFull(*S)){
+        ((*S).TOP)++;
+        (*S).T[(*S).TOP]=X; 
+    }
+}
+
+void Pop(Stack *S, infotype *X){
+/* Menghapus X dari Stack S. */
+/* I.S. S tidak kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+    if(!IsEmpty(*S)){
+        *X = (*S).T[(*S).TOP];
+        ((*S).TOP)--;
+    }
+}
+
+
+/* ********** Operator Tambahan ********* */
+void ForcePush(Stack *S, infotype X)
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, S mungkin penuh */
+/* F.S. X menjadi element TOP yang baru, TOP bertambah 1
+        Apabila S penuh, buang element paling bawah dari S dan masukkan X sebagai TOP
+        Contoh: S berisi a b c d e f g h i j, setelah melakukan ForcePush(S, "k")
+        S berisi b c d e f g h i j k */
+        // KAMUS
+{
+        Stack Stemp;
+        infotype L;
+        infotype del; // untuk menyimpan buangan elemen paling bawah
+        // ALGORITMA
+        CreateEmpty(&Stemp);
+        if (IsFull(*S)){
+            while((*S).TOP != 0){
+                Pop(S, &L);
+                Push(&Stemp, L);
+            }
+            Pop(S, &del); //elemenpaling bawah
+            while(!IsEmpty(Stemp)){
+                Pop(&Stemp, &L);
+                Push(S, L);
+            }
+        }
+        Push(S, X);
+}
+
+void InverseStack (Stack *S)
+/* Membalik isi suatu Stack */
+/* I.S. S terdefinisi */
+/* F.S. Isi S terbalik dari posisi semula */
+{
+    infotype X;
+    if (!IsEmpty(*S))
+    {
+        Pop(S, &X);
+        InverseStack(S);
+        Push(S, X);
+    }
+}
+
+void PrintStack (Stack S)
+/* I.S. S terdefinisi, mungkin kosong */ 
+/* F.S. Isi S tertulis di layar dari bottom ke top (hasil inverse). */ 
+/* Di akhir penulisan, S kembali ke kondisi I.S. */ 
+/* Jika S kosong, tuliskan “Stack kosong” */ 
+{
+    infotype X;
+    InverseStack(&S);
+    while (!IsEmpty(S))
+    {
+        Pop(&S, &X);
+        printf("%d", X);
+    }
+    InverseStack(&S);
+}
+
---
+++ stack.h
@@ -0,0 +1,70 @@
+
+#ifndef stack_H
+#define stack_H
+#include <stdio.h>
+#include "komponen.h"
+#include "boolean.h"
+
+#define Nil 0
+#define MaxEl 10
+
+typedef Komponen infotype;
+typedef int address;
+
+typedef struct
+{
+    infotype T[MaxEl];
+    address TOP;
+} Stack;
+
+/* Definisi stack S kosong : S.TOP = Nil */
+/* S.TOP = jumlah element stack */
+/* S.T[S.TOP-1] = element paling atas */
+
+/* ********* Prototype ********* */
+
+/* *** Konstruktor/Kreator *** */
+void CreateEmpty(Stack *S);
+/* I.S. Sembarang */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* Ciri stack kosong : TOP bernilai Nil */
+
+/* ********* Predikat Untuk test keadaan KOLEKSI ********* */
+bool IsEmpty(Stack S);
+/* Mengirim true jika Stack kosong*/
+/* Ciri stack kosong : TOP bernilai Nil */
+bool IsFull(Stack S);
+/* Mengirim true jika stack S penuh */
+/* Ciri stack penuh : TOP bernilai MaxEl */
+
+/* ********** Operator Dasar Stack ********* */
+void Push(Stack *S, infotype X);
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, S tidak penuh */
+/* F.S. X menjadi element TOP yang baru, TOP bertambah 1 */
+void Pop(Stack *S, infotype *X);
+/* Menghapus X dari Stack S. */
+/* I.S. S tidak kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+
+/* ********** Operator Tambahan ********* */
+void ForcePush(Stack *S, infotype X);
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, S mungkin penuh */
+/* F.S. X menjadi element TOP yang baru, TOP bertambah 1
+        Apabila S penuh, buang element paling bawah dari S dan masukkan X sebagai TOP
+        Contoh: S berisi a b c d e f g h i j, setelah melakukan ForcePush(S, "k")
+        S berisi b c d e f g h i j k */
+
+void InverseStack(Stack *S);
+/* Membalik isi suatu Stack */
+/* I.S. S terdefinisi */
+/* F.S. Isi S terbalik dari posisi semula */
+
+void PrintStack (Stack S);
+/* I.S. S terdefinisi, mungkin kosong */ 
+/* F.S. Isi S tertulis di layar dari bottom ke top (hasil inverse). */ 
+/* Di akhir penulisan, S kembali ke kondisi I.S. */ 
+/* Jika S kosong, tuliskan “Stack kosong” */ 
+
+#endif
